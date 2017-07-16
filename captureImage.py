690eb965f2e807786e21e6ca8d5c2214ad1023b1
AjeetGitHub2016 <er.ajeetsinghyadav@gmail.com>
2017-07-16 13:25:38 +0530

    Update captureImage.py

--- captureImage.py
+++ captureImage.py
@@ -3,5 +3,18 @@ from picamera import PiCamera
 from time import sleep
 from picamera.array import PiRGBArray
 import CV2
-# Capture the images
-
+# camera setting
+camera = PiCamera()
+camera.resolution = (256, 256)
+rawCapture = PiRGBArray(camera, size=(256, 256))
+# wait time
+camera.start_preview()
+sleep(1)
+# capture image
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+camera.stop_preview()
+# save and display image
+cv2.imwrite('image.jpg',image)
+cv2.imshow("Image", image)
+cv2.waitKey(0)
